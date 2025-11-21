03caf0d7ecfb15cf30f7b99cc6dc74e38fe296e9
oumaima hatim <oumaimahatim143@gmail.com>
2025-11-21 21:40:20 +0100

    landinpage

---
+++ src/main/java/com/myapp/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.myapp.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import com.myapp.model.User;
+
+public class MainController {
+
+    @FXML
+    private Label label;
+
+    private User user = new User("Zakaria");
+
+    @FXML
+    public void handleClick() {
+        label.setText("Salam " + user.getName() + "!");
+    }
+}
