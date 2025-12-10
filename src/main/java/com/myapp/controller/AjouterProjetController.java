5524c6395cb6dd4acd90b06075357c6d6d7791f5
hp <tamrifatima276@gmail.com>
2025-12-10 22:30:20 +0100

    hh

---
+++ src/main/java/com/myapp/controller/AjouterProjetController.java
@@ -0,0 +1,62 @@
+package com.myapp.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class AjouterProjetController {
+
+    @FXML
+    private TextField nomProjetField;
+
+    @FXML
+    private TextField clientField;
+
+    @FXML
+    private TextField villeField;
+
+    @FXML
+    private TextField adresseField;
+
+    @FXML
+    private ComboBox<String> etatCombo;
+
+    @FXML
+    private TextArea descriptionArea;
+
+    @FXML
+    private Button enregistrerButton;
+
+    @FXML
+    private Button retourButton;
+
+    @FXML
+    public void initialize() {
+        // تعمير combo état مبدئياً
+        if (etatCombo != null) {
+            etatCombo.getItems().addAll("En cours", "Terminé", "En pause");
+        }
+    }
+
+    @FXML
+    private void onEnregistrerClicked() {
+        // test – ghir nchofo l-form kaykhddem
+        System.out.println("Projet = " + nomProjetField.getText());
+        System.out.println("Client = " + clientField.getText());
+        System.out.println("Ville = " + villeField.getText());
+        System.out.println("Adresse = " + adresseField.getText());
+        System.out.println("Etat = " + etatCombo.getValue());
+        System.out.println("Description = " + descriptionArea.getText());
+        // men b3d tzid DAO / DB
+    }
+
+    @FXML
+    private void onRetourClicked() {
+        // إغلاق الفورم
+        Stage stage = (Stage) retourButton.getScene().getWindow();
+        stage.close();
+    }
+}
